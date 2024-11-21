cc00cd99b973bccfb87d431e8fbafc3f24fd3403
조현지 <qndjddl001@gmail.com>
2024-11-21 13:21:15 +0900

    feat: 해당 게시글 댓글 전체 조회 기능 구현
    게시글 선택 후 게시글에 포함된 댓글 전체 조회
    - 생성일자 기준으로 정렬

--- src/main/java/com/example/bugfixhub/service/post/PostService.java
+++ src/main/java/com/example/bugfixhub/service/post/PostService.java
@@ -2,6 +2,8 @@ package com.example.bugfixhub.service.post;
 
 import com.example.bugfixhub.dto.post.*;
 
+import java.util.List;
+
 public interface PostService {
     PostResDto create(Long userId, PostReqDto postReqDto);
 
@@ -10,6 +12,8 @@ public interface PostService {
     GetIdPostResDto getPostById(Long id);
 
     PostResDto update(Long id, Long userId, PostReqDto postReqDto);
+
+    List<PostCommentsResDto> getPostComments(Long id);
 
     void delete(Long id, Long userId);
 }
