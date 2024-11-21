58f5d0594e5c9e410cd9086888e3d4af2987ad5c
beunyeong <oo1409@kakao.com>
2024-11-21 21:39:37 +0900

    fix: 코드 수정

--- src/main/java/com/example/bugfixhub/dto/likes/CommentLikeResDto.java
+++ src/main/java/com/example/bugfixhub/dto/likes/CommentLikeResDto.java
@@ -1,5 +1,6 @@
 package com.example.bugfixhub.dto.likes;
 
+import com.example.bugfixhub.entity.comment.Comment;
 import lombok.*;
 
 @Data
@@ -7,17 +8,16 @@ public class CommentLikeResDto {
 
 
     private final Long id;
-    private final Long userId;
-    private final String content;     // 댓글 내용
-    private final Long likeCount;     // 좋아요 수
-    private final boolean isLiked;    // 사용자가 좋아요 했는지 확인
 
-    private CommentLikeResDto(Long id, Long userId, String content, Long likeCount, boolean isLiked) {
-        this.id = id;
-        this.userId = userId;
-        this.content = content;
-        this.likeCount = likeCount;
-        this.isLiked = isLiked;
+    private final Long userId;
+
+    private final String commentId;
+
+
+    public CommentLikeResDto(Comment comment) {
+        this.id = comment.getId();
+        this.userId = comment.getUser().getId();
+        this.commentId = comment.getId().toString();
 
     }
 
